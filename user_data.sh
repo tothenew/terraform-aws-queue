23bad0a245ae88e36e21ec4ce813db641822b302
nitinpainoli <nitin.nn711@gmail.com>
2023-01-10 13:47:21 +0530

    fixing rabbit version 3.10

--- user_data.sh
+++ user_data.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 yum update -y
-amazon-linux-extras install epel -y
-yum install erlang -y
-yum install rabbitmq-server -y
+#amazon-linux-extras install epel -y
+#yum install erlang -y
+#yum install rabbitmq-server -y
+wget https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.1/erlang-23.2.1-1.el7.x86_64.rpm
+yum localinstall erlang-23.2.1-1.el7.x86_64.rpm -y
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
+yum -y install rabbitmq-server
+systemctl enable --now rabbitmq-server.service
 rabbitmq-plugins enable rabbitmq_management
-systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
-systemctl stop rabbitmq-server
+systemctl enable rabbitmq-server.service
+systemctl start rabbitmq-server.service
+systemctl stop rabbitmq-server.service
 truncate -s 0  /var/lib/rabbitmq/.erlang.cookie
 echo "XAIFUIBJAVHSEZOKOMHD" >>  /var/lib/rabbitmq/.erlang.cookie 
-systemctl start rabbitmq-server
+systemctl start rabbitmq-server.service
 export USERNAME="$(aws ssm get-parameter --name /${environment_name}/rabbit/USERNAME --with-decryption --output text --query Parameter.Value --region ${region})"
 echo "$USERNAME"
 export PASS="$(aws ssm get-parameter --name /${environment_name}/rabbit/PASSWORD --with-decryption --output text --query Parameter.Value --region ${region})"
@@ -20,4 +25,4 @@ sudo rabbitmqctl set_user_tags admin administrator
 sudo rabbitmqctl set_permissions -p / admin ".*" ".*" ".*"
 sleep 10s
 sudo rabbitmq-plugins enable rabbitmq_management
-sudo systemctl restart rabbitmq-server
+sudo systemctl restart rabbitmq-server.service
--- worker.sh
+++ worker.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 sudo yum update -y
-sudo yum install nc -y
-sudo amazon-linux-extras install epel -y
-sudo yum install erlang -y
-sudo yum install rabbitmq-server -y
+# sudo yum install nc -y
+# sudo amazon-linux-extras install epel -y
+# sudo yum install erlang -y
+wget https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.1/erlang-23.2.1-1.el7.x86_64.rpm
+yum localinstall erlang-23.2.1-1.el7.x86_64.rpm -y
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
+yum -y install rabbitmq-server
+systemctl enable --now rabbitmq-server.service
 sudo rabbitmqctl start_app
 sudo rabbitmqctl stop_app
 sudo truncate -s 0  /var/lib/rabbitmq/.erlang.cookie
@@ -15,6 +19,8 @@ sudo rabbitmqctl start_app
 sudo rabbitmqctl stop_app
 export MASTER_IP="$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${Name}" "Name=instance-state-name,Values=running"  --query 'Reservations[*].Instances[*].{PrivateIP:PrivateIpAddress}' --output text --region ${region})"
 echo "$MASTER_IP"
+yum install -y nc
+
 while [ $? -eq 0 ]
 do
     nc -zv "$MASTER_IP" 4369

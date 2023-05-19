4519bdc77485ab58cb2f49e6899b384bc9494371
Harsh Vardhan <126136590+harvar31@users.noreply.github.com>
2023-05-19 12:45:57 +0530

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -28,3 +28,40 @@ sudo rabbitmq-plugins enable rabbitmq_management
 sudo systemctl restart rabbitmq-server.service
 rabbitmq-plugins enable rabbitmq_prometheus
 sudo systemctl restart rabbitmq-server.service
+
+wget https://github.com/prometheus/node_exporter/releases/download/v1.5.0/node_exporter-1.5.0.linux-amd64.tar.gz
+tar xvfz node_exporter-1.5.0.linux-amd64.tar.gz
+
+# Move Node Exporter binaries
+sudo mv node_exporter-1.5.0.linux-amd64/node_exporter /usr/local/bin/
+
+# Create a system user for Node Exporter
+sudo useradd -rs /bin/false node_exporter
+
+# Create a systemd service file for Node Exporter
+sudo tee /etc/systemd/system/node_exporter.service << EOF
+[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=default.target
+EOF
+
+# Set ownership and permissions
+sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
+sudo chmod +x /usr/local/bin/node_exporter
+
+# Reload systemd and start Node Exporter
+sudo systemctl daemon-reload
+sudo systemctl start node_exporter
+
+# Enable Node Exporter to start on system boot
+sudo systemctl enable node_exporter
+sudo systemctl status node_exporter

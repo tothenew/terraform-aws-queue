427c382e26ee65405089f0a716248b655b8db2f1
Harsh Vardhan <126136590+Harshvardhanttn@users.noreply.github.com>
2023-06-07 17:24:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -103,6 +103,7 @@ data "template_file" "user_data" {
   vars = {
     environment_name = var.environment_name
     region           = var.region
+    master_user_data = file(var.master_user_data_path == "" ? "" : var.master_user_data_path)
   }
 }
 
@@ -143,6 +144,7 @@ data "template_file" "user_data_worker" {
     environment_name = var.environment_name
     region           = var.region
     Name             = "${var.project_name_prefix}-Rabbit-MQ-Master"
+    worker_user_data = file(var.worker_user_data_path == "" ? "" : var.worker_user_data_path)
   }
 }
 
